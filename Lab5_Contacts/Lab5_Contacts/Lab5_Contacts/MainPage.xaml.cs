39158f4022be11c5fbbaec3f6c2f5ff24c10ae96
Yevheniy Romanenko <zerom2016romanenko@gmail.com>
2020-04-21 20:55:51 +0300

    add lab 5 - work with contacts

---
+++ Lab5_Contacts/Lab5_Contacts/Lab5_Contacts/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using Lab5_Contacts.Views;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Lab5_Contacts
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void CheckAllContacts_click(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new AllContacts());
+        }
+
+        private async void CheckFilteredContacts_click(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new AllContacts(x => !string.IsNullOrWhiteSpace(x.Number) && x.Number.Contains("050")));
+        }
+    }
+}
