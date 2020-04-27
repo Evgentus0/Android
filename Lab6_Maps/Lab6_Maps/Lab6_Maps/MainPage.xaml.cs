97dc5f8a4e9d65baa0bc14e92e7e2c9b0b8156eb
Yevheniy Romanenko <zerom2016romanenko@gmail.com>
2020-04-28 02:28:36 +0300

    add finding distance

--- Lab6_Maps/Lab6_Maps/Lab6_Maps/MainPage.xaml.cs
+++ Lab6_Maps/Lab6_Maps/Lab6_Maps/MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using Plugin.Geolocator;
+﻿using Lab6_Maps.Views;
+using Plugin.Geolocator;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -19,140 +20,47 @@ namespace Lab6_Maps
         {
             InitializeComponent();
 
-            Init();
 
+            Init();
         }
 
-        private async void Init()
+        private void Init()
         {
-            var map = await InitMap();
-
-            var grid = InitGrid();
-
-            grid.Children.Add(map, 0, 0);
-
-            var panel = await GetPanel();
-
-            grid.Children.Add(panel, 0, 1);
+            var grid = GetGrid();
 
             Content = grid;
-
-            await UpdateCordinate(panel, map);
-
         }
 
-        private async Task UpdateCordinate(Grid panel, Map map)
-        {
-            while (true)
-            {
-                await Task.Delay(5000);
-
-                var position = await GetPosition();
-                
-                var longth = position.Longitude;
-                var width = position.Latitude;
-
-                var coordinate = (Label)panel.Children[1];
-
-                coordinate.Text = $"Latitude: {width}; Longitude: {longth}";
-
-                map.MoveToRegion(MapSpan.FromCenterAndRadius(new Position(position.Latitude, position.Longitude),
-                                             Distance.FromMiles(0.7)));
-            }
-        }
-
-        private async Task<Map> InitMap()
-        {
-            var position = await GetPosition();
-
-            var longth = position.Longitude;
-
-            var width = position.Latitude;
-
-            Position cubic = new Position(width, longth);
-            MapSpan mapSpan = new MapSpan(cubic, 0.01, 0.01);
-
-            Map map = new Map(mapSpan)
-            {
-                IsShowingUser = true,
-                Scale = 1
-            };
-
-            return map;
-        }
-
-        private Grid InitGrid()
+        private Grid GetGrid()
         {
             var grid = new Grid();
-            grid.RowDefinitions.Add(new RowDefinition() { Height = new GridLength(0.8, GridUnitType.Star) });
-            grid.RowDefinitions.Add(new RowDefinition() { Height = new GridLength(0.2, GridUnitType.Star) });
 
-            grid.ColumnDefinitions.Add(new ColumnDefinition() { Width = GridLength.Star });//{Width = GridLength.Auto});
+            grid.RowDefinitions.Add(new RowDefinition() { Height = GridLength.Auto });
+            grid.RowDefinitions.Add(new RowDefinition() { Height = GridLength.Auto });
+
+            grid.ColumnDefinitions.Add(new ColumnDefinition() { Width = GridLength.Star });
+
+            var currentLocationButton = new Button() { Text = "Current Location" };
+            currentLocationButton.Clicked += GetCurrentLocationButton_click;
+
+            var findLocation = new Button() { Text = "Find Location" };
+            findLocation.Clicked += FindLocationButton_click;
+
+            grid.Children.Add(currentLocationButton, 0, 0);
+            grid.Children.Add(findLocation, 0, 1);
 
             return grid;
         }
-
-        private async Task<Grid> GetPanel()
-        {
-            var grid = new Grid();
-            grid.RowDefinitions.Add(new RowDefinition() { Height = GridLength.Star });
-            grid.RowDefinitions.Add(new RowDefinition() { Height = GridLength.Star });
-
-            grid.ColumnDefinitions.Add(new ColumnDefinition() { Width = GridLength.Star });
-            grid.ColumnDefinitions.Add(new ColumnDefinition() { Width = GridLength.Star });
-
-            grid.Children.Add(new Label() { Text = "Current coordinate: " }, 0, 0);
-
-
-            var position = await GetPosition();
-            var longth = position.Longitude;
-            var width = position.Latitude;
-
-            grid.Children.Add(new Label() { Text = $"Latitude: {width}; Longitude: {longth}" }, 1, 0);
-
-            Button getCurrentLocationButton = new Button() { Text = "Get current location" };
-            getCurrentLocationButton.Clicked += GetCurrentLocationButton_click;
-
-            grid.Children.Add(getCurrentLocationButton, 0, 1);
-
-            Geocoder geoCoder = new Geocoder();
-
-            Position positionAdress = new Position(width, longth);
-            IEnumerable<string> possibleAddresses = await geoCoder.GetAddressesForPositionAsync(positionAdress);
-            string address = possibleAddresses.FirstOrDefault();
-
-            grid.Children.Add(new Label() { Text = address }, 1, 1);
-
-            return grid;
-        }
-
 
         private async void GetCurrentLocationButton_click(object sender, EventArgs args)
         {
-            var position = await GetPosition();
-            var longth = position.Longitude;
-            var width = position.Latitude;
-
-            Geocoder geoCoder = new Geocoder();
-            Position positionAdress = new Position(width, longth);
-            IEnumerable<string> possibleAddresses = await geoCoder.GetAddressesForPositionAsync(positionAdress);
-            string address = possibleAddresses.FirstOrDefault();
-
-            var content = (Grid)Content;
-
-            var panel = (Grid)content.Children[1];
-
-            var label = (Label)panel.Children[3];
-
-            label.Text = address;
+            await Navigation.PushAsync(new CurrentLocation());
         }
 
-        private async Task<Plugin.Geolocator.Abstractions.Position> GetPosition()
+        private async void FindLocationButton_click(object sender, EventArgs args)
         {
-            var locator = CrossGeolocator.Current;
-            var position = await locator.GetPositionAsync(new TimeSpan(10000));
-
-            return position;
+            await Navigation.PushAsync(new FindLocation());
         }
+
     }
 }
